4954c5713ed333ad527c7f0fb4830665d99498b8
cesarvalencia <cesar.valencia49@gmail.com>
2020-10-22 10:01:25 -0400

    changes

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,12 +2,26 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class HelloworldApplication{
+@RestController
+public class HelloworldApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args){
         SpringApplication.run(HelloworldApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloworldApplication.class);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World from Tomcat,,,,,,";
+    }
 }
